76167c8e4f31d76103a0c88e4c9663d3dd1c88cc
capo <capo@debian-BULLSEYE-live-builder-AMD64>
2023-02-21 10:37:45 +0300

    Homepage

---
+++ pages/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="bg- dark:bg-gray-900 overflow-hidden">
+        <header class="bg-blue-900 text-white">
+            <nav class="flex items-center justify-between container mx-auto py-4">
+                <a href="#" class="font-bold text-lg">My Portfolio</a>
+                <ul class="flex items-center">
+                    <li><a href="#" class="px-3 py-2">Home</a></li>
+                    <li><a href="#" class="px-3 py-2">About</a></li>
+                    <li><a href="#" class="px-3 py-2">Portfolio</a></li>
+                    <li><a href="#" class="px-3 py-2">Contact</a></li>
+                </ul>
+            </nav>
+            <div class="container mx-auto py-20 text-center">
+                <h1 class="text-4xl font-bold">Capo Bambino</h1>
+                <h2 class="text-xl font-bold">FullStack Web Developer</h2>
+            </div>
+        </header>
+
+        <main class="container mx-auto py-10">
+            <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
+                <div class="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-5">
+                    <h3 class="text-2xl font-bold">About Me</h3>
+                    <p class="text-gray-600 dark:text-gray">Lorem ipsum dolor</p>
+                </div>
+            </div>
+        </main>
+    </div>
+</template>
+<script setup>
+
+</script>
+<style lang="">
+    
+</style>
