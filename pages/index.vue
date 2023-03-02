850f0e6c282372e354e2f0c34bf902721105fa87
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-03-02 12:37:34 +0300

    edit

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,30 @@
 <template>
-    <div class="bg- dark:bg-gray-900 overflow-hidden">
-        <header class="bg-blue-900 text-white">
-            <nav class="flex items-center justify-between container mx-auto py-4">
-                <a href="#" class="font-bold text-lg">My Portfolio</a>
-                <ul class="flex items-center">
-                    <li><a href="#" class="px-3 py-2">Home</a></li>
-                    <li><a href="#" class="px-3 py-2">About</a></li>
-                    <li><a href="#" class="px-3 py-2">Portfolio</a></li>
-                    <li><a href="#" class="px-3 py-2">Contact</a></li>
-                </ul>
-            </nav>
-            <div class="container mx-auto py-20 text-center">
-                <h1 class="text-4xl font-bold">Capo Bambino</h1>
-                <h2 class="text-xl font-bold">FullStack Web Developer</h2>
+    <div class="h-screen bg-gray-300 dark:bg-gray-900">
+        <header class="text-gray-600 body-font">
+            <div class="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
+                <a class="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round"
+                        stroke-linejoin="round" stroke-width="2" class="w-10 h-10 p-2 text-white bg-purple-500 rounded-full"
+                        viewBox="0 0 24 24">
+                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                    </svg>
+                    <span class="ml-3 text-xl">capo bambino</span>
+                </a>
+                <nav class="flex flex-wrap items-center justify-center text-base md:ml-auto">
+                    <a class="mr-5 hover:text-gray-900">First Link</a>
+                    <a class="mr-5 hover:text-gray-900">Second Link</a>
+                    <a class="mr-5 hover:text-gray-900">Third Link</a>
+                    <a class="mr-5 hover:text-gray-900">Fourth Link</a>
+                </nav>
+                <button
+                    class="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0">Button
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                        class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                        <path d="M5 12h14M12 5l7 7-7 7"></path>
+                    </svg>
+                </button>
             </div>
         </header>
-
-        <main class="container mx-auto py-10">
-            <div class="grid grid-cols-1 md:grid-cols-2 gap-10">
-                <div class="bg-white dark:bg-gray-800 shadow-lg rounded-lg p-5">
-                    <h3 class="text-2xl font-bold">About Me</h3>
-                    <p class="text-gray-600 dark:text-gray">Lorem ipsum dolor</p>
-                </div>
-            </div>
-        </main>
     </div>
 </template>
 <script setup>
