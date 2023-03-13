b853e73cd8d9cfcdc61fbb4e97b6a359d27965b5
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-03-13 13:18:01 +0300

    added some SEO and Meta tags in Nuxt Config :)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,5 +14,26 @@ export default defineNuxtConfig({
   // 
   //
   // 
-  nitro: { prerender: { routes: ['/', '/portfolio', '/about'] } }
+  nitro: { prerender: { routes: ['/', '/portfolio', '/about'] } },
+
+  // SEO and Meta
+  app: {
+    head: {
+      charset: 'utf-8',
+      viewport: 'width=device-width, initial-scale=1',
+      title: 'Charles Wahome',
+      meta: [
+        // <meta name="description" content="Charles Wahome is a software engineer and a web developer.">
+        { name: 'description', content: 'Charles Wahome is a software engineer and a web developer.' },
+        { name: 'keywords', content: 'Charles Wahome,  capo bambino, capo, bambino, Charles, Kariuki, Wahome, Charles Kariuki, Charles Kariuki Wahome ,software engineer, web developer, web designer, web developer in Kenya, software engineer in Kenya, web designer in Kenya' },
+        { name: 'author', content: 'Charles Wahome' },
+        { name: 'robots', content: 'index, follow' },
+        { name: 'googlebot', content: 'index, follow' },
+        { name: 'google', content: 'nositelinkssearchbox' },
+        { name: 'google', content: 'notranslate' },
+        { name: 'google', content: 'notranslate' },
+      ],
+    },
+  },
+
 })
