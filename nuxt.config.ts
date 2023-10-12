c52adf2730b2eecd094ee1dc2728c53bc8984424
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-10-12 18:22:43 +0300

    Update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,17 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'nuxt-icon', '@nuxtjs/google-fonts'],
+  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode', 'nuxt-icon', 
+  //'@nuxtjs/google-fonts'
+  ],
   colorMode: {
     preference: '', // default theme
     dataValue: 'theme', // activate data-theme specified in layouts/default.vue
     fallback: '', // fallback theme
   },
-  googleFonts: {
-    families: {
-      'Bungee+Shade': true,
-      'Raleway': true,
-    },
-  },
+  // googleFonts: {
+  //   families: {
+  //     'Bungee+Shade': true,
+  //     'Raleway': true,
+  //   },
+  // },
   // 
   // SEO and Meta
   app: {
@@ -21,12 +23,11 @@ export default defineNuxtConfig({
       title: 'Charles Wahome',
       meta: [
         { name: 'description', content: 'Charles Wahome is a software engineer and a web developer.' },
-        { name: 'keywords', content: 'Charles Wahome,  capo bambino, capo, bambino, Charles, Kariuki, Wahome, Charles Kariuki, Charles Kariuki Wahome ,software engineer, web developer, web designer, web developer in Kenya, software engineer in Kenya, web designer in Kenya' },
+        { name: 'keywords', content: 'Charles Wahome, charleswahome,  capo bambino, capo, bambino, Charles, Kariuki, Wahome, Charles Kariuki, Charles Kariuki Wahome ,software engineer, web developer, web designer, web developer in Kenya, software engineer in Kenya, web designer in Kenya' },
         { name: 'author', content: 'Charles Wahome' },
         { name: 'robots', content: 'index, follow' },
         { name: 'googlebot', content: 'index, follow' },
         { name: 'google', content: 'nositelinkssearchbox' },
-        { name: 'google', content: 'notranslate' },
         { name: 'google', content: 'notranslate' },
       ],
     },
