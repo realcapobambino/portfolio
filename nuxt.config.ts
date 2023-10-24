81c1d0c8b95ae73a649c3cabc48c07f541d7f0d6
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-10-24 14:38:27 +0300

    fix favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,6 +21,7 @@ export default defineNuxtConfig({
       charset: 'utf-8',
       viewport: 'width=device-width, initial-scale=1',
       title: 'Charles Wahome',
+      link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicons/favicon.ico' }],
       meta: [
         { name: 'description', content: 'Charles Wahome is a software engineer and a web developer.' },
         { name: 'keywords', content: 'Charles Wahome, charleswahome,  capo bambino, capo, bambino, Charles, Kariuki, Wahome, Charles Kariuki, Charles Kariuki Wahome ,software engineer, web developer, web designer, web developer in Kenya, software engineer in Kenya, web designer in Kenya' },
--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,3 +1,16 @@
 <template>
     <h1>BLOG</h1>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: `Bambino's Blog`,
+  meta: [
+    { name: 'description', content: `Bambino's Blog.`}
+  ]
+//   bodyAttrs: {
+//     class: 'test'
+//   },
+//   script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
