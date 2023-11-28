8624320509f78bedd6425cf300d0aa91132f854c
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-11-28 17:15:53 +0300

    removed store

--- src/store/mutation-types.ts
+++
@@ -1,7 +0,0 @@
-export enum MutationTypes {
-    SET_USER = 'SET_USER',
-    UNSET_USER = 'UNSET_USER',
-    SET_TOKEN = 'SET_TOKEN',
-    UNSET_TOKEN = 'UNSET_TOKEN',
-
-}
--- src/store/state.ts
+++
@@ -1,7 +0,0 @@
-import { User } from "@/App/Common/Types/User"
-
-
-export const state = {
-    user: <User>JSON.parse(localStorage.getItem('user') || '{}'),
-
-}
