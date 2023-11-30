1cda9581786a9973eeb4187ac05930d81eb75e1b
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-11-30 16:49:26 +0300

    cleaned main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,20 +3,8 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 import Vue3Lottie from 'vue3-lottie'
-// import { inject } from '@vercel/analytics';
-import VueMeta from 'vue-meta'
 
-
-
-
-// vercel analytics
-// inject();
-//
-
-//
 const app = createApp(App)
     .use(router)
     .use(Vue3Lottie)
-    .use(VueMeta)
-
     .mount('#app')
