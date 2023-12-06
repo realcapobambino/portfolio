6feb8735745d419722bdbc5f5285e9589eae63c4
realcapobambino <35867381+realcapobambino@users.noreply.github.com>
2023-12-06 18:26:53 +0300

    fixed gitignore and added posthog plugin

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import './assets/tailwind.css'
 import Vue3Lottie from 'vue3-lottie'
+import posthogPlugin from "./plugins/posthog";
 
 const app = createApp(App)
     .use(router)
     .use(Vue3Lottie)
+    .use(posthogPlugin)
     .mount('#app')
